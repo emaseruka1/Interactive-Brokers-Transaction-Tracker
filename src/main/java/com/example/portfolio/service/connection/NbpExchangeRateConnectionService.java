6795941a4463f06236f48f31b2dca5310ac82e1b
emaseruka1 <emmamaseruka97@gmail.com>
2026-01-08 12:45:43 +0100

    fixed NBP error 404 no data issue on holidays

--- src/main/java/com/example/portfolio/service/connection/NbpExchangeRateConnectionService.java
+++ src/main/java/com/example/portfolio/service/connection/NbpExchangeRateConnectionService.java
@@ -7,6 +7,9 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -15,24 +18,31 @@ public class NbpExchangeRateConnectionService {
 
     public NbpExchangeRateConnectionService(){}
 
-    public BigDecimal getNbpInterbankPlnUsdRate(String date) {
+    public BigDecimal getNbpInterbankPlnUsdRate(String ibkrTradedate) {
 
-        date = date.substring(0, 4) + "-" + date.substring(4, 6) + "-" + date.substring(6);
-        String url = "https://api.nbp.pl/api/exchangerates/rates/A/USD/" + date + "/?format=json";
-        HttpClient client = HttpClient.newHttpClient();
+        LocalDate dateIterator = LocalDate.parse(ibkrTradedate, DateTimeFormatter.ofPattern("yyyyMMdd"));
 
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+        while(true){
 
-        try{
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Trying to fetch NBP fx Rate for date: "+dateIterator);
+            String date = dateIterator.toString();
+            String url = "https://api.nbp.pl/api/exchangerates/rates/A/USD/" + date + "/?format=json";
+            HttpClient client = HttpClient.newHttpClient();
 
-            JSONObject json = new JSONObject(response.body());
-            JSONArray rates = json.getJSONArray("rates");
-            double midRate = rates.getJSONObject(0).getDouble("mid");
-            return BigDecimal.valueOf(midRate);
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
 
-        } catch (Exception e) {
-            return BigDecimal.ZERO;
+            try{
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+                JSONObject json = new JSONObject(response.body());
+                JSONArray rates = json.getJSONArray("rates");
+                double midRate = rates.getJSONObject(0).getDouble("mid");
+                return BigDecimal.valueOf(midRate);
+
+            } catch (Exception e) {
+
+                dateIterator = dateIterator.minusDays(1);
+            }
         }
     }
 }
