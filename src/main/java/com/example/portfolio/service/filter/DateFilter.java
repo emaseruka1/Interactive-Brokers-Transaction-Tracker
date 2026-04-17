fcd85c31ed86d940da9fca73e40b874f670eee9d
emaseruka1 <emmamaseruka97@gmail.com>
2026-04-17 05:03:40 +0200

    fix null pointer

--- src/main/java/com/example/portfolio/service/filter/DateFilter.java
+++ src/main/java/com/example/portfolio/service/filter/DateFilter.java
@@ -71,6 +71,8 @@ public class DateFilter {
 
         List<JsonNode> tradesFilteredByDate = new ArrayList<>();
 
+        log.info("trades from getAllTrades: {}",ibkrFlexJsonDataModal.getAllTrades());
+
         for (JsonNode trd:ibkrFlexJsonDataModal.getAllTrades()){
 
             for (int i=0; i<trd.size();i++){
